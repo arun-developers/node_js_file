e7967a663ddfeb1f3da275ddcb5e90b160e5bf46
arun-developers <aruns3435@gmail.com>
2022-01-31 21:54:27 +0530

    commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// for (i=0,i<5,i++) {
+//     console.log(i);
+
+// }
+
+// var a = 10;
+// var b = 20;
+// var c = a+b;
+
+// console.log(c);
+// console.log('arun singh');
+
+
+// const a = 10;
+
+// const b = 16;
+// let c = a*b;
+// console.log(c); 
+
+// var a = 12;
+// var b = 15;
+// var c = a**b;
+// console.log(c);
+
+// const a = "HELLO WORLD";
+
+// console.log(a);
+
+// const fs = require("fs");
+
+// fs.writeFileSync("read.txt","welcome to my notes");
+
+// fs.appendFileSync("read.txt","To add something to my file"); 
+
+// fs.readFileSync("read.txt");
+// const buf_data = fs.readFileSync("read.txt");
+// // console.log(buf_data);
+// org_data = buf_data.toString();
+// console.log(org_data);
+
+// To rename the file name ;
+
+// fs.renameSync("read.txt","readwrite.txt");
+
+// CURD operation using file system module in nodeJs ;
+
+// 1; create a folder it named arun ;
+// 2; create a file in it named Bio.txt and data into it. 
+// 3; add more data into the file at the end of the existing data . 
+// 4; read the data without getting the buffer data at first (hint; to use file encoding);
+// 5; rename the file name to mybio . txt 
+// 6; now delete both the file and the folder .
+
